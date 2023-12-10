1bab9ac53ecfa9e9ea832775efadb611e714ea45
Wulinwei <wu.linwei@outlook.com>
2023-12-10 22:09:55 +0800

    leptjson.c:2

---
+++ leptjson.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "leptjson.h"
+typedef struct
+{
+    const char *json;
+} lept_context;
+
+// 提示：这里应该是 JSON-text = ws value ws
+// 以下实现未处理最后的ws和LEPT_PARSE_ROOT_NOT_SINGULAR
+int lept_parse(lept_value *value, const char *json)
+{
+    lept_context c;
+    assert(v != NULL);
+    c.json = json;
+    v.type = LEPT_NULL;
+    lept_parse_whitespace(&c);
+    return lept_parse_value(&c, v);
+}
