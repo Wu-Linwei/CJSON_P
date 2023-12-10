80bb7798a068d03a5b77a31f5e60c227b13d12b7
Wulinwei <wu.linwei@outlook.com>
2023-12-10 21:13:11 +0800

    第一次编写

---
+++ lrptjson.h
@@ -0,0 +1,23 @@
+#ifndef LRPTJSON_H__
+#define LRPTJSON_H__
+
+typedef enum
+{
+    LEPT_NULL,
+    LEPT_FALSE,
+    LEPT_TRUE,
+    LEPT_NUMBER,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+} lept_type;
+
+typedef struct
+{
+    lept_type type;
+} lept_value;
+
+int lept_parse(lept_value *value, const char *json);
+
+lept_type lept_get_value(const lept_value *value);
+#endif // LRPTJSON_H__
