48875a39c5cc688cbe12a6acfbb0ad9509fa71fb
Wulinwei <wu.linwei@outlook.com>
2023-12-20 20:08:21 +0800

    第二次编写

--- leptjson.h
+++ leptjson.h
@@ -17,6 +17,14 @@ typedef struct
     lept_type type;
 } lept_value;
 
+enum
+{
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+
 int lept_parse(lept_value *value, const char *json);
 
 lept_type lept_get_value(const lept_value *value);
