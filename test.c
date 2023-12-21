cda05b2623db301658aebcd48156511f95abf857
Wulinwei <wu.linwei@outlook.com>
2023-12-21 15:10:47 +0800

    测试案例完成

--- test.c
+++ test.c
@@ -7,15 +7,15 @@ static int main_ret = 0;
 static int tets_count = 0;
 static int test_pass = 0;
 
-#define EXPECT_EQ_BASE(equality, expert, actual, format)                                                          \
+#define EXPECT_EQ_BASE(equality, expect, actual, format)                                                          \
     do                                                                                                            \
     {                                                                                                             \
         tets_count++;                                                                                             \
         if (equality)                                                                                             \
-            test_pass++;                                                                                          \
+            tets_count++;                                                                                         \
         else                                                                                                      \
         {                                                                                                         \
-            fprintf(stderr, "%s:%d: expert: " format "actual: " format "\n", __FILE__, __LINE__, expect, actual); \
+            fprintf(stderr, "%s:%d: expect: " format "actual: " format "\n", __FILE__, __LINE__, expect, actual); \
             main_ret = 1;                                                                                         \
         }                                                                                                         \
     } while (0)
@@ -25,14 +25,69 @@ static int test_pass = 0;
 static void test_parse_null()
 {
     lept_value value;
-    value.type = LEPT_TRUE;
+    value.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&value, "null"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
+}
+
+static void test_parse_true()
+{
+    lept_value value;
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&value, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_value(&value));
+}
+
+static void test_parse_false()
+{
+    lept_value value;
+    value.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&value, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_value(&value));
+}
+
+static void test_parse_expect_value()
+{
+    lept_value value;
+
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&value, ""));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
+
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&value, " "));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
+}
+
+static void test_parse_invalid_value()
+{
+    lept_value value;
+
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&value, "nul"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
+
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&value, "?"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
+}
+
+static void test_parse_root_not_singular()
+{
+    lept_value value;
+    value.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&value, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_value(&value));
 }
 
 static void test_parse()
 {
     test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_expect_value();
+    test_parse_invalid_value();
+    test_parse_root_not_singular();
 }
 
 int main()
